223db312f7169b8a66be767e7f7f1abce07fd9ed
Jan Dudek <jd@jandudek.com>
2015-08-06 01:01:10 +0200

    Extract Verification module

--- ElmerPress.elm
+++ ElmerPress.elm
@@ -1,37 +1,46 @@
 module ElmerPress where
 
-import Debug
-import Random
-import Time
-import Signal.Extra
 import Http
-import Task exposing (Task, succeed, fail, andThen, onError)
+import Html exposing (Html)
+import Random
+import Signal.Extra
+import Task exposing (Task)
+import Time
 
 import ElmerPress.Game as Game
 import ElmerPress.View.Game as GameView
 import ElmerPress.Action as Action exposing (..)
+import ElmerPress.Verification as Verification
 
+main : Signal Html
 main =
+  Signal.map view models
+
+models : Signal Game.Model
+models =
   let
-    update = Game.update
-
-    view = GameView.view actionsAddress
-
-    inputs = Signal.map2 (,) actions.signal seeds
-
-    models =
-      let
-        updateFromInput ((Just action), seed) model = update action model
-      in
-        Signal.Extra.foldp' updateFromInput (Game.initModel << snd) inputs
+    updateFromInput ((Just action), seed) model = Game.update action model
   in
-    Signal.map view models
+    Signal.Extra.foldp' updateFromInput (Game.initModel << snd) inputs
 
+inputs : Signal (Maybe Action, Random.Seed)
+inputs =
+  let
+    allActions =
+      Signal.merge actions.signal verification.actions
+  in
+    Signal.map2 (,) allActions seeds
+
+view =
+  GameView.view actions.address
+
+actions : { signal : Signal (Maybe Action), address : Signal.Address Action }
 actions =
-  Signal.mailbox Nothing
-
-actionsAddress =
-  Signal.forwardTo actions.address Just
+  let
+    mailbox = Signal.mailbox Nothing
+    address = Signal.forwardTo mailbox.address Just
+  in
+    { mailbox | address <- address }
 
 seeds : Signal Random.Seed
 seeds =
@@ -41,62 +50,10 @@ seeds =
   in
     Signal.map (Random.initialSeed << round << ((*) 1000)) timeSignal
 
-wordQueries =
-  let
-    isQuery action =
-      case action of
-        Just (Query _) -> True
-        _ -> False
+verification :
+  { actions : Signal (Maybe Action), tasks : Signal (Task Http.Error ()) }
+verification =
+  Verification.fromActions actions.signal
 
-    toJustWord action =
-      case action of
-        Just (Query word) -> Just word
-        Nothing -> Nothing
-  in
-    Signal.map toJustWord (Signal.filter isQuery Nothing actions.signal)
-
-wordRequestResults : Signal.Mailbox (String, Bool)
-wordRequestResults =
-  Signal.mailbox ("", False)
-
-knownWords : Signal (List (String, Bool))
-knownWords =
-  Signal.foldp (::) [] wordRequestResults.signal
-
-requestWord word =
-  let
-    url =
-      "http://letterpress-words-api.herokuapp.com/" ++ Http.uriEncode(word)
-
-    succeedIf200 _ =
-      succeed (word, True)
-
-    succeedIf404 err =
-      case err of
-        Http.BadResponse 404 _ -> succeed (word, False)
-        _ -> fail err
-
-    request =
-      (Http.getString url `andThen` succeedIf200) `onError` succeedIf404
-
-    sendResult result =
-      Signal.send wordRequestResults.address result
-  in
-    request `andThen` sendResult
-
-port wordRequestTasks : Signal (Task Http.Error ())
-port wordRequestTasks =
-  let
-    doRequest query =
-      case query of
-        Nothing ->
-          succeed ()
-
-        Just word ->
-          requestWord word
-  in
-    Signal.map doRequest wordQueries
-
-port actionsFromKnownWords : Signal (Task x ())
-port actionsFromKnownWords =
-  Signal.map (\list -> Signal.send actionsAddress (Verified list)) knownWords
+port runTasks : Signal (Task Http.Error ())
+port runTasks = verification.tasks
